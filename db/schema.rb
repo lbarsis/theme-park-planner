accfbb170f2a236457a97e529b9453be9574cbb7
Levi Barsis <lbarsis@gmail.com>
2023-03-06 18:03:20 -0800

    Added Admin to Users and updated seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_18_173238) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_014916) do
   create_table "itineraries", force: :cascade do |t|
     t.string "name"
     t.integer "group_size"
@@ -68,6 +68,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_18_173238) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "itineraries", "users"
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,32 +6,58 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 # create theme parks
-disney_world = ThemePark.create(name: "Walt Disney World", city: "Orlando", state: "Florida", description: "The most magical place on earth")
+# disney_world = ThemePark.create(name: "Walt Disney World", city: "Orlando", state: "Florida", description: "The most magical place on earth")
+# universal_orlando = ThemePark.create(name: "Universal Orlando Resort", city: "Orlando", state: "Florida", description: "Rides for the daring!")
+
+# # create rides
+# pirates = Ride.create(name: "Pirates of the Caribbean", thrill_level: 3, duration: 15, capacity: 16, description: "Embark on a swashbuckling voyage to a long-forgotten time and place when pirates and privateers ruled the seas.", theme_park: disney_world)
+# haunted = Ride.create(name: "Haunted Mansion", thrill_level: 2, duration: 10, capacity: 20, description: "Board your doom buggy and travel through the chambers of the eerie estate, home to ghosts, ghouls and supernatural surprises.", theme_park: disney_world)
+# hulk = Ride.create(name: "The Incredible Hulk Coaster", thrill_level: 5, duration: 2, capacity: 32, description: "Hang on tight for a thrilling ride as you rocket from zero to 40 mph in just two seconds flat.", theme_park: universal_orlando)
+# rip_ride = Ride.create(name: "Hollywood Rip Ride Rockit", thrill_level: 4, duration: 3, capacity: 24, description: "Blast off on a high-speed journey that loops, twists and dives around the park.", theme_park: universal_orlando)
+
+# # create users
+# alice = User.create(name: "Alice", username: 'al1', email: "alice@example.com", password: "password")
+# bob = User.create(name: "Bob", username: 'bo1',email: "bob@example.com", password: "password")
+
+# # create itineraries
+# itinerary1 = Itinerary.create(name: "Disney Vacation", group_size: 2, start_date: Date.new(2023, 3, 20), end_date: Date.new(2023, 3, 26), user: alice)
+# itinerary2 = Itinerary.create(name: "Universal Trip", group_size: 2, start_date: Date.new(2023, 4, 15), end_date: Date.new(2023, 4, 20), user: bob)
+
+# # create itinerary rides
+# ItineraryRide.create(itinerary: itinerary1, ride: pirates)
+# ItineraryRide.create(itinerary: itinerary1, ride: haunted)
+# ItineraryRide.create(itinerary: itinerary2, ride: hulk)
+# ItineraryRide.create(itinerary: itinerary2, ride: rip_ride)
+
+# # create ride_users
+# RideUser.create(ride: pirates, user: alice)
+# RideUser.create(ride: pirates, user: bob)
+# RideUser.create(ride: haunted, user: alice)
+# RideUser.create(ride: hulk, user: bob)
+# RideUser.create(ride: rip_ride, user: bob)
+
+
+# create theme parks
+magic_kingdom = ThemePark.create(name: "Magic Kingdom", city: "Orlando", state: "FL", description: "The most magical place on earth!")
+disneyland = ThemePark.create(name: "Disneyland", city: "Anaheim", state: "CA", description: "Where the magic began!")
 universal_orlando = ThemePark.create(name: "Universal Orlando Resort", city: "Orlando", state: "Florida", description: "Rides for the daring!")
 
 # create rides
-pirates = Ride.create(name: "Pirates of the Caribbean", thrill_level: 3, duration: 15, capacity: 16, description: "Embark on a swashbuckling voyage to a long-forgotten time and place when pirates and privateers ruled the seas.", theme_park: disney_world)
-haunted = Ride.create(name: "Haunted Mansion", thrill_level: 2, duration: 10, capacity: 20, description: "Board your doom buggy and travel through the chambers of the eerie estate, home to ghosts, ghouls and supernatural surprises.", theme_park: disney_world)
+pirates_of_caribbean = Ride.create(name: "Pirates of the Caribbean", thrill_level: 2, duration: 8, capacity: 16, description: "Sail through a pirate-infested Caribbean town", theme_park: magic_kingdom)
+haunted_mansion = Ride.create(name: "Haunted Mansion", thrill_level: 1, duration: 10, capacity: 20, description: "Take a spooky tour of a haunted house", theme_park: magic_kingdom)
+space_mountain = Ride.create(name: "Space Mountain", thrill_level: 4, duration: 5, capacity: 12, description: "Blast off into space on a thrilling roller coaster", theme_park: magic_kingdom)
+it_s_a_small_world = Ride.create(name: "It's a Small World", thrill_level: 1, duration: 15, capacity: 24, description: "A gentle boat ride around the world", theme_park: magic_kingdom)
+splash_mountain = Ride.create(name: "Splash Mountain", thrill_level: 3, duration: 9, capacity: 18, description: "Plunge down a steep water slide", theme_park: magic_kingdom)
 hulk = Ride.create(name: "The Incredible Hulk Coaster", thrill_level: 5, duration: 2, capacity: 32, description: "Hang on tight for a thrilling ride as you rocket from zero to 40 mph in just two seconds flat.", theme_park: universal_orlando)
 rip_ride = Ride.create(name: "Hollywood Rip Ride Rockit", thrill_level: 4, duration: 3, capacity: 24, description: "Blast off on a high-speed journey that loops, twists and dives around the park.", theme_park: universal_orlando)
 
 # create users
-alice = User.create(name: "Alice", username: 'al1', email: "alice@example.com", password: "password")
-bob = User.create(name: "Bob", username: 'bo1',email: "bob@example.com", password: "password")
+admin = User.create(name: "Admin User", username: "admin", email: "admin@example.com", password: "password", admin: true)
+user1 = User.create(name: "John Smith", username: "johnsmith", email: "john@example.com", password: "password")
+user2 = User.create(name: "Jane Doe", username: "janedoe", email: "jane@example.com", password: "password")
 
 # create itineraries
-itinerary1 = Itinerary.create(name: "Disney Vacation", group_size: 2, start_date: Date.new(2023, 3, 20), end_date: Date.new(2023, 3, 26), user: alice)
-itinerary2 = Itinerary.create(name: "Universal Trip", group_size: 2, start_date: Date.new(2023, 4, 15), end_date: Date.new(2023, 4, 20), user: bob)
+itinerary1 = Itinerary.create(name: "Magical Vacation", group_size: 4, start_date: Date.today + 1, end_date: Date.today + 7, user: user1, rides:[pirates_of_caribbean, haunted_mansion, space_mountain])
+itinerary2 = Itinerary.create(name: "Disney Adventure", group_size: 2, start_date: Date.today + 2, end_date: Date.today + 8, user: user2, rides:[it_s_a_small_world, splash_mountain])
 
-# create itinerary rides
-ItineraryRide.create(itinerary: itinerary1, ride: pirates)
-ItineraryRide.create(itinerary: itinerary1, ride: haunted)
-ItineraryRide.create(itinerary: itinerary2, ride: hulk)
-ItineraryRide.create(itinerary: itinerary2, ride: rip_ride)
 
-# create ride_users
-RideUser.create(ride: pirates, user: alice)
-RideUser.create(ride: pirates, user: bob)
-RideUser.create(ride: haunted, user: alice)
-RideUser.create(ride: hulk, user: bob)
-RideUser.create(ride: rip_ride, user: bob)
