815c4ffa68212c485d91276aa3fafecb2a9dec7e
Levi Barsis <lbarsis@gmail.com>
2023-03-16 19:09:46 -0700

    removed theme_park from itinerary_serializer

--- app/serializers/itinerary_serializer.rb
+++ app/serializers/itinerary_serializer.rb
@@ -1,8 +1,4 @@
 class ItinerarySerializer < ActiveModel::Serializer
-  attributes :id, :name, :group_size, :start_date, :end_date, :rides, :theme_park
+  attributes :id, :name, :group_size, :start_date, :end_date, :rides
   has_one :user
-
-  def theme_park
-    theme_park = ThemePark.find(object.rides.first.theme_park_id)
-  end
 end
